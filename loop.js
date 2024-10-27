a0b3d1e91c3d622266c66eb4acfcb7099afdccf9
Sami0189 <samisaimon28@gmail.com>
2024-10-28 01:33:02 +0600

    loop

---
+++ loop.js
@@ -0,0 +1,24 @@
+const mobile={
+    brand:'samsumg',
+    price:130000,
+    color:'black',
+    camera:'120mp'
+}
+
+
+
+//for of :array
+//for in :object
+// for(const prop in mobile){
+//     console.log(prop)
+//     console.log(mobile[prop])
+// }
+
+
+const keys=Object.keys(mobile);
+console.log(keys);
+
+
+for(const key of keys){
+    console.log(key,':',mobile[key])
+}
