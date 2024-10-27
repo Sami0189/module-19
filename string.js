4725494011ac061d907f3f3a3eed096ce99fe2e4
Sami0189 <samisaimon28@gmail.com>
2024-10-27 18:36:38 +0600

    string

---
+++ string.js
@@ -0,0 +1,22 @@
+const country = 'Bangladesh';
+const division = 'new Cumilla';
+const district = `Barishal`;
+// const thana = new String('Demra');
+
+// console.log(thana);
+
+
+const numbers = [53,64,75,34,743];
+// console.log(numbers.length);
+// console.log(numbers[1]);
+numbers[1] = 43;
+console.log(numbers);
+
+//string is immutable thats means you cannot change it .
+//array is mutable thats means you can change it.
+const capital = 'Dhaka';
+// console.log(capital.length);
+// console.log(capital[4]);
+capital[2] = 'd';
+console.log(capital);
+
