c6d8fc22ec634c1bac9da2d073309a4f3024fafe
Sami0189 <samisaimon28@gmail.com>
2024-10-28 00:08:34 +0600

    object

---
+++ object.js
@@ -0,0 +1,22 @@
+const age = 21;
+const school = 'rifle aquare';
+const isPassed = true;
+const subjects = ['bangla','english','physics','math'];
+const bottleWrong = ['white',45];
+
+//object
+const bottle = {
+   brand:'apple',
+   price:54,
+   color:'white',
+   isClean:false
+}
+
+const subjects= {
+    name:'biology',
+    teacher:'sami',
+    examDate:'30 feb',
+    chapter:['first','second','third','fourth'],
+    exam='final exam'
+}
+
