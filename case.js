0a13491ad68e83f8eed59e377784aa04c2b8c95a
Sami0189 <samisaimon28@gmail.com>
2024-10-27 19:31:56 +0600

    case

---
+++ case.js
@@ -0,0 +1,35 @@
+const school = 'Raj uk Uttara Model School';
+
+console.log(school.toLowerCase());
+console.log(school);
+console.log(school.toUpperCase());
+
+// const subject = 'chemistry';
+// const book = 'chemistry';
+
+// if(subject === book){
+//     console.log('i am reading book aibar parikkhai fatai felmo');
+// }
+// else{
+//     console.log('hudai hudai prista ultai ar vat khawar jonno');
+// }
+
+
+const subject = 'Chemistry';
+const book = 'chemistry';
+
+if(subject.toLowerCase() === book.toLowerCase()){
+    console.log('i am reading book aibar parikkhai fatai felmo');
+}
+else{
+    console.log('hudai hudai prista ultai ar vat khawar jonno');
+}
+
+const drink = ' water  ';
+const liquid = '  water  ';
+if(drink.trim() === liquid.trim()){
+    console.log('koi');
+}
+else{
+    console.log('amar');
+}
