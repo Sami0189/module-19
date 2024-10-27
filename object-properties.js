dad21bcb222c3b40856e4343a922fe9b18ab3e5d
Sami0189 <samisaimon28@gmail.com>
2024-10-28 00:39:11 +0600

    object-properties

---
+++ object-properties.js
@@ -0,0 +1,53 @@
+// const person = {
+//     name:'sami',
+//     age:43,
+//     salary:53400,
+//     married:true,
+//     profession:'web-developer'
+// }
+//dot symbol diye object er property er value access kora
+
+//console.log(person);
+// console.log(person.age);
+
+// const income= person.salary;
+// console.log(income);
+
+
+
+
+
+// const person = {
+//     name:'sami',
+//     age:43,
+//     salary:53400,
+//     married:true,
+//     profession:'web-developer',
+//     'fav places':['bandorban','sainmartin','kuakata']
+// }
+// console.log(person['age']);
+// const boyos =person['age'];
+// console.log(boyos)
+// console.log(person["fav places"])
+
+
+
+
+
+
+
+// value
+const person = {
+    name:'sami',
+    age:43,
+    salary:53400,
+    married:true,
+    profession:'web-developer',
+    'fav places':['bandorban','sainmartin','kuakata']
+}
+
+
+person.salary=80000;
+person['age']=34;
+person['fav places']= 'cumilla';
+console.log(person)
